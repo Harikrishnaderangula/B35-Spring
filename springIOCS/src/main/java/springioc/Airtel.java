3bb03701f71e11b55d5e158d856748b3b79bf07a
Harikrishna <harikrishnaderangula214@gmail.com>
2023-02-19 15:59:15 +0530

    First commit

---
+++ springIOCS/src/main/java/springioc/Airtel.java
@@ -0,0 +1,20 @@
+package springioc;
+
+public class Airtel implements Sim {
+	
+	
+	public Airtel() {
+		System.out.println("Airtel Class Constructor");
+	}
+
+	public void call() {
+		System.out.println(" I am airtel calling");
+		
+	}
+
+	public void data() {
+		System.out.println(" I am using data");
+		
+	}
+
+}
---
+++ springIOCS/src/main/java/springioc/Sim.java
@@ -0,0 +1,8 @@
+package springioc;
+
+//to demonstrate a program on spring IOC
+public interface Sim {
+	//abstract method
+	void call();
+	void data();
+}
